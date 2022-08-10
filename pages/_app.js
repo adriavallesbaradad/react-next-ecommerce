e921cc93b7156210857ed9b13a7f136160e9ca02
Adrià Vallès Baradad <100840351+adriavallesbaradad@users.noreply.github.com>
2022-08-10 16:33:38 +0200

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import Link from "next/link";
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Component {...pageProps} />
+        <Footer />
+      </>
+  )
 }
 
 export default MyApp
